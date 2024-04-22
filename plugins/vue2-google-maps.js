0a69d7bbe2f19bcfb99d503b45e05d257656f780
Dustin Jason Lee <dustin.jason.lee@gmail.com>
2024-04-22 10:17:09 -0400

    resolve security issues

--- plugins/vue2-google-maps.js
+++ plugins/vue2-google-maps.js
@@ -5,12 +5,12 @@ import * as VueGoogleMaps from "~/node_modules/vue2-google-maps/dist/main.js"
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: "AIzaSyBBb0pba3vw7x1JXTM_umY012djw0mMpSM",
+    key: process.env.GOOGLE_API,
     libraries: "places",
   },
   installComponents: true,
 })
 
 Vue.use(VuetifyGoogleAutocomplete, {
-  apiKey: "AIzaSyBBb0pba3vw7x1JXTM_umY012djw0mMpSM",
+  apiKey: process.env.GOOGLE_API,
 })
